0fa8c5cbb76496955d31a2dfb194ad70bb6ef566
JuanMaCarrearas <manuucarreras@gmail.com>
2023-11-08 20:40:16 -0300

    index

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 import app from './src/app'
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hola Mundo desde el Back</h1>')
-})
-
 app.listen(3000, () => {
     console.log('Server lintener on port ', 3000)
 })
