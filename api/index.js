e1a5d5cbde6c65d4a36faee62805a1ccbae851d1
JuanMaCarrearas <manuucarreras@gmail.com>
2023-11-30 21:27:38 -0300

    index

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import app from './src/app.js'
 import sequelize from './src/db.js'
-import './src/models/products.js'
+import './src/models/Products.js'
 
 sequelize.sync({ force: true }).then(() => {
     console.log('Data base connected')
