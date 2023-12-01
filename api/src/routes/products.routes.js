2252c8f08d8d83544cf811489d720486437a5d22
JuanMaCarrearas <manuucarreras@gmail.com>
2023-11-30 22:07:05 -0300

    add search by name Route

--- api/src/routes/products.routes.js
+++ api/src/routes/products.routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express'
 import {
-    getProducts,
+    getAllProducts,
     createProducts,
+    searchProducts,
 } from '../controllers/products.controller.js'
 
 const router = Router()
 
-router.get('/products', getProducts)
+router.get('/products', getAllProducts)
+router.get('/products/:id', searchProducts)
 router.post('/products', createProducts)
 router.put('/products/:id')
 router.delete('/products/:id')
-router.get('/products/:id')
 
 export default router
