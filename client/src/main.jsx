593f2c6430db2f66a65d103ba72d59be72e38624
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-05 20:33:09 -0300

    react-router-dom

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './redux/store.js'
+// import { Provider } from 'react-redux'
+// import store from './redux/store.js'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-  <React.StrictMode>
+  // <Provider store={store}>
+    <Router>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </Router>
+  // </Provider>
 )
