d402665e0937378b708c11e11de5fe12a1a25786
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-04 21:50:36 -0300

    Redux

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,13 +1,12 @@
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
-    extends: [
-        'standard',
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:react/jsx-runtime',
-        'plugin:react-hooks/recommended',
-    ],
+    // extends: [
+    //     'standard',
+    //     'plugin:react/recommended',
+    //     'plugin:react/jsx-runtime',
+    //     'plugin:react-hooks/recommended',
+    // ],
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: {
         ecmaVersion: 'latest',
