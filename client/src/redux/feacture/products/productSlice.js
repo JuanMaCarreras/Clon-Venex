d78da3e68c4b1b7ac87299380ad4b1173844a394
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-05 21:30:27 -0300

    redux-toolkit

---
+++ client/src/redux/feacture/products/productSlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const productSlice = createSlice({
+    name: 'products',
+    initialState: [],
+    reducers: {},
+})
+
+export default productSlice.reducer
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import reducer from './reducer'
+import productsReducer from './feacture/products/productSlice'
 
 const store = configureStore({
-    reducer,
+    products: productsReducer,
 })
 
 export default store
