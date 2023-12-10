3827bee0bc63b361c3c69b1786acc22e3701249a
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-09 22:13:08 -0300

    add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import morgan from 'morgan'
 import express from 'express'
+import cors from 'cors'
 import productsRouters from './routes/products.routes.js'
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 app.use(productsRouters)
 
