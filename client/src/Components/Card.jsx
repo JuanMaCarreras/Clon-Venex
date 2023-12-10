b262d4928c85375eecd0557d14d64130638f15e6
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-10 18:43:21 -0300

    the data does not show

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,18 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { fetchProducts } from '../redux/slices/productSlice'
 
 function Card() {
 
     const dispatch = useDispatch()
-    const products = useSelector(state => state.products.data)
-
-    console.log(products)
+    const products = useSelector(state => {
+      console.log('Estado actualizado:', state.products.data)
+      return state.products.data
+    })
 
     useEffect(() => {
 
       dispatch(fetchProducts())
-      console.log( products)
+        .catch(error => {
+          console.error('Error fetching products in component:', error)
+        })
 
     },[dispatch])
 
@@ -20,6 +23,23 @@ function Card() {
   return (
     <>
       <h1>Productos</h1>
+
+
+
+      {
+        products ? (
+          <ul>
+            {
+              products.map( (product) => {
+                <li key={products.name}>{product.name}</li>
+                
+              })
+            }
+          </ul>
+        ) : (
+          <p>cagando...</p>
+        )
+      }
     
     
 
