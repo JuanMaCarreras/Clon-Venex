ec7dd9c37a171b087ee8148f84fa3bcc23d7bf71
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-14 18:41:53 -0300

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
 // import Home from './components/Home'
 import './App.css'
 import Card from './components/Card'
-import Data from './components/Data'
 
 function App () {
   return (
     <>
       {/* <Home /> */}
-      {/* <Card /> */}
-      <Data />
       <Card />
     </>
   )
