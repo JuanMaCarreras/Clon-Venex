1abf087a5ca6920aabecd6ff792dc61afc9ae4a9
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-15 22:11:53 -0300

    products list

---
+++ client/src/Components/CardList.jsx
@@ -0,0 +1,11 @@
+import Data from './Data'
+
+function CardList () {
+  return (
+    <div className='flex flex-row flex-wrap border border-blue-700'>
+      <Data />
+    </div>
+  )
+}
+
+export default CardList
