e2dd7444e5ecf6ccb60c96557681f26b05599842
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-18 21:13:22 -0300

    add style

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -7,14 +7,13 @@ function Card ({ product }) {
 
   return (
     <>
-      <div className='flex flex-col items-center justify-center border-red-600 m-5 py-3 border'>
+      <div className='flex flex-col items-center justify-center  border-red-600 m-5 py-3 border'>
         <div className='bg-grey-600'>
           <img src={img} alt='img' className='h-52 w-52 ' />
         </div>
-        <div className=''>
-          <h2 className='text-xl'>${price}</h2>
-          <h3 className='text-base text-center  text-gray-500 truncate w-[500px]'>{name}</h3>
-        </div>
+        <h2 className='text-xl'>${price}</h2>
+        <p className='text-base  text-gray-500 truncate w-[500px]'>{name}</p>
+        <span className=''>hola</span>
       </div>
     </>
   )
