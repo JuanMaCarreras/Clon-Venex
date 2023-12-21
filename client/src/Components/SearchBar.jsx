1b960f2033048f70f0d4258b85ba0c8f6b21a9e5
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-20 21:43:47 -0300

    search bar

--- client/src/Components/SearchBar.jsx
+++ client/src/Components/SearchBar.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 function SearchBar () {
+  const dispatch = useDispatch()
+
+  const [search, setSearch] = useState('')
+
+  const handlerChange = (e) => {
+    const input = e.target.value
+    console.log(input)
+    setSearch(input)
+  }
+
+  const handlerSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <h2>Search Bar</h2>
+      <form onSubmit={handlerSubmit}>
+        <input
+          name='search'
+          type='text'
+          onChange={handlerChange}
 
-      <input name='search' />
-      <label />
+        />
+
+        <button type='submit'>Buscar</button>
+      </form>
 
     </>
   )
