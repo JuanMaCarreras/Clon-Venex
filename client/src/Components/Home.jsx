23a6f642682f6087af085246501da9a433137a55
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-21 21:41:50 -0300

    add search bar

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,9 +1,12 @@
 import Footer from './Footer'
 import CardList from './CardList'
+import SearchBar from './SearchBar'
 
 function Home () {
   return (
     <>
+      <SearchBar />
+
       <CardList />
       <h1>hola soy el homeASDASDAQWRSF asdsadwe asdqwraxc asdwqrwq</h1>
       <Footer />
