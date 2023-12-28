a53094650ead58ee8d22a2475fcd90cac1b24361
JuanMaCarrearas <manuucarreras@gmail.com>
2023-12-27 21:29:28 -0300

    create product form

--- client/src/Components/CreateProduct.jsx
+++ client/src/Components/CreateProduct.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+// import { useEffect, useState } from 'react'
 
 function CreateProduct () {
+  const handleInputChange = (e) => {
+    e.preventDefault()
+  }
+
+  const handleSubmit = () => {
+
+  }
+
   return (
     <>
-      <form action='' />
+      <form onSubmit={handleSubmit}>
+
+        <label>Name:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        <label>Price:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        {/* <label>img:</label>
+        <input type='text' /> */}
+
+        <label>Description:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        <label>Category:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        <label>Brand:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        <label>Stock:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+
+        <label>Availability:</label>
+        <input
+          type='text'
+          onChange={handleInputChange}
+        />
+      </form>
     </>
   )
 }
