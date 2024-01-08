05fe475f649a732f3c0a8fca023486a7f70cfa76
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-07 21:51:19 -0300

    add crete products reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsReducer from './slices/productSlice'
+import createProductsSlice from './slices/privateCreateProducts'
 
 const store = configureStore({
     reducer: {
         products: productsReducer,
-    },
+        createProducts: createProductsSlice
+    }
 })
 
 export default store
