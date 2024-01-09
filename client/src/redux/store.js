6ba2f5069154941a7dc82c39b532225c6081a171
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-08 21:41:59 -0300

    remove create product reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productsReducer from './slices/productSlice'
-import createProductsSlice from './slices/privateCreateProducts'
 
 const store = configureStore({
     reducer: {
-        products: productsReducer,
-        createProducts: createProductsSlice
+        products: productsReducer
     }
 })
 
