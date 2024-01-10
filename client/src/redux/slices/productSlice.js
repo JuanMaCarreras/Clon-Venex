dcf6e78e40b3cece09c2cf25e6ce5ddc37b8634b
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-10 20:35:06 -0300

    modularized actions

--- client/src/redux/slices/productSlice.js
+++ client/src/redux/slices/productSlice.js
@@ -1,25 +1,5 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import { getAllProducts, searchProducts } from '../../api/api'
-
-export const fetchProducts = createAsyncThunk('/products/fetchProducts', async () => {
-  try {
-    const res = await getAllProducts()
-    return res
-  } catch (error) {
-    console.log(error)
-    throw error
-  }
-}
-)
-
-export const searchProduct = createAsyncThunk('/products/searchProducts', async (productName, { rejectedWithValue }) => {
-  try {
-    const res = await searchProducts(productName)
-    return res
-  } catch (error) {
-    return rejectedWithValue(error)
-  }
-})
+import { createSlice } from '@reduxjs/toolkit'
+import { fetchProducts, searchProduct } from '../actions/productsActions'
 
 const initialState = {
   data: [],
