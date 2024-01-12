2e805117b919148ec50cefc0c7cd0b3364b627e0
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-11 21:15:05 -0300

    create products action

--- client/src/redux/actions/productsActions.js
+++ client/src/redux/actions/productsActions.js
@@ -1,22 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
-import { getAllProducts, searchProducts } from '../../api/api'
+import { getAllProducts, searchProducts, createProducts } from '../../api/api'
 
 export const fetchProducts = createAsyncThunk('/products/fetchProducts', async () => {
-    try {
-        const res = await getAllProducts()
-        return res
-    } catch (error) {
-        console.log(error)
-        throw error
-    }
+  try {
+    const res = await getAllProducts()
+    return res
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 }
 )
 
 export const searchProduct = createAsyncThunk('/products/searchProducts', async (productName, { rejectedWithValue }) => {
-    try {
-        const res = await searchProducts(productName)
-        return res
-    } catch (error) {
-        return rejectedWithValue(error)
-    }
+  try {
+    const res = await searchProducts(productName)
+    return res
+  } catch (error) {
+    return rejectedWithValue(error)
+  }
+})
+
+export const productsForm = createAsyncThunk('/creteProducts', async (productData) => {
+  try {
+    const res = await createProducts(productData)
+    return res
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
 })
