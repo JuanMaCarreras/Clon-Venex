ba6e36558e37320aae26873fac0a0ce1f38a2178
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-13 21:33:00 -0300

    integrate Auth0

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,22 @@ import morgan from 'morgan'
 import express from 'express'
 import cors from 'cors'
 import productsRouters from './routes/products.routes.js'
+import { auth } from 'express-openid-connect'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
+
+const { CLIENT_ID, SECRET, DOMAIN } = process.env
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: { SECRET },
+    baseURL: 'http://localhost:3000',
+    clientID: { CLIENT_ID },
+    issuerBaseURL: { DOMAIN }
+}
 
 // Middleware
 app.use(morgan('dev'))
