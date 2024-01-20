b377f79bc5ac25bdb234313627d5fdc0cdf19746
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-20 18:09:47 -0300

    route to detail component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import CreateProducts from './components/CreateProduct'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
+import ProductDetails from './components/ProductDetails.jsx'
 
 function App () {
   return (
@@ -8,6 +9,7 @@ function App () {
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/create_products' element={<CreateProducts />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
       </Routes>
     </>
   )
