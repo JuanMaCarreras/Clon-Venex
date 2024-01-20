dfa08fbac69cbc62d7eec936085190ec11b45b28
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-20 18:08:16 -0300

    details component

---
+++ client/src/Components/ProductDetails.jsx
@@ -0,0 +1,13 @@
+import { useSelector } from 'react-redux'
+import idProduct from '../redux/actions/productsActions'
+
+function ProductDetails () {
+  const id = useSelector()
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default ProductDetails
