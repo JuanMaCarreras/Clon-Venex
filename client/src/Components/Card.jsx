38effddfd4fea98612ed504fab9653e2aeb86f16
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-20 18:10:23 -0300

    Link to detail component

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -1,4 +1,6 @@
-function Card ({ product }) {
+import { Link } from 'react-router-dom'
+
+function Card ({ product, id }) {
   if (!product) {
     return null
   }
@@ -7,11 +9,13 @@ function Card ({ product }) {
 
   return (
     <>
-      <article className='flex flex-col items-center justify-center border-red-600 m-5 py-5 border-'>
-        <img src={img} alt='img' className='h-52 w-52 ' />
-        <h2 className='text-2xl font-medium'>${price}</h2>
-        <h3 className='text-sm text-gray-500 max-w-xl'>{name}</h3>
-      </article>
+      <Link to={`/products/${id}`}>
+        <article className='flex flex-col items-center justify-center border-red-600 m-5 py-5 border-'>
+          <img src={img} alt='img' className='h-52 w-52 ' />
+          <h2 className='text-2xl font-medium'>${price}</h2>
+          <h3 className='text-sm text-gray-500 max-w-xl'>{name}</h3>
+        </article>
+      </Link>
     </>
   )
 }
