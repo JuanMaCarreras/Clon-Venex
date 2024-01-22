1286d60b84499e4a568e21a923e95a8e83b351e0
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-21 21:54:19 -0300

    details

--- client/src/Components/ProductDetails.jsx
+++ client/src/Components/ProductDetails.jsx
@@ -1,11 +1,25 @@
-import { useSelector } from 'react-redux'
-import idProduct from '../redux/actions/productsActions'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
+import { getProductById } from '../redux/actions/productsActions'
 
 function ProductDetails () {
-  const id = useSelector()
+  const dispatch = useDispatch()
+  const details = useSelector(state => state.products.details)
+  const { productid } = useParams()
+  console.log(productid)
+  console.log(useParams())
+
+  useEffect(() => {
+    dispatch(getProductById(productid))
+  }, [dispatch, productid])
 
   return (
     <>
+      {
+        details && <h1>{ProductDetails.name}</h1>
+
+      }
     </>
   )
 }
