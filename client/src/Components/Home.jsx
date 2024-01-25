3c23326557b2e776bb2ad4afa0d88fd20e846324
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-25 20:18:00 -0300

    add slider

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,13 +1,15 @@
 import Footer from './Footer'
 import CardList from './CardList'
 import NavBar from './NavBar'
+import SliderImg from './SliderImg'
 
 function Home () {
   return (
     <>
       <NavBar />
+      <SliderImg />
       <CardList />
-      <h1>hola soy el homeASDASDAQWRSF asdsadwe asdqwraxc asdwqrwq</h1>
+      <h1>HOLA SOY EL HOME</h1>
       <Footer />
     </>
   )
