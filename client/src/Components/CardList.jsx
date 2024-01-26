318d679b30890a369ae0478e9c0ff522dd77a14e
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-26 18:06:26 -0300

    border style

--- client/src/Components/CardList.jsx
+++ client/src/Components/CardList.jsx
@@ -2,7 +2,7 @@ import Data from './Data'
 
 function CardList () {
   return (
-    <div className='flex flex-row flex-wrap border border-blue-700'>
+    <div className='flex flex-row flex-wrap'>
       <Data />
     </div>
   )
