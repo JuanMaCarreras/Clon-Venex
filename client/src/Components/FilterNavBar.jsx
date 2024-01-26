46b24fb1aaf47e50a4b0ceca4cb5a2a74160e991
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-25 21:58:50 -0300

    filter bar style

---
+++ client/src/Components/FilterNavBar.jsx
@@ -0,0 +1,19 @@
+function FilterNavBar () {
+  return (
+    <>
+      <ul className='flex flex-row justify-between py-mypd px-14'>
+        <li className='font-semibold'>CATEGORÍAS</li>
+        <li>PCs de Escritorio</li>
+        <li>Notebooks</li>
+        <li>Placas de Videos</li>
+        <li>Monitores</li>
+        <li>Micros</li>
+        <li>¡Imperdibles!</li>
+        <li>|Arma tu PC|</li>
+        <li className='font-semibold'>VX | CORPORATIVO</li>
+      </ul>
+    </>
+  )
+}
+
+export default FilterNavBar
