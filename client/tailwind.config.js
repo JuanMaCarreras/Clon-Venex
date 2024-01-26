5b79b20abeeb086693087c74ace2e682e5eaa147
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-25 23:09:35 -0300

    size config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,8 +11,17 @@ export default {
         secondaryGray: '#2d2d33',
         venex: '#91c612'
 
+      },
+      height: {
+        300: '353px',
+        90: '10rem'
+      },
+      padding: {
+        mypd: '16px',
+        'py-20': '20px',
+        'py-30': '30px'
       }
-    }
-  },
-  plugins: []
+    },
+    plugins: []
+  }
 }
