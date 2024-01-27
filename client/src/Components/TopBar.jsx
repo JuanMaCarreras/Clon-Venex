dafc7f6b5c6fa436526790b13758723d836dbf2a
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-27 19:51:05 -0300

    font

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -9,8 +9,8 @@ function TopBar () {
         className='flex flex-row justify-end py-1.5 bg-secondaryGray'
       >
         <ul className='flex text-white'>
-          <li><FaRoute className='w-4 h-6 text-venex' /></li>
-          <li className='pr-14 pl-3'> Seguí tu compra  </li>
+          <li><FaRoute className='w-4 h-6 text-venex ' /></li>
+          <li className='pr-14 pl-3 font-serif'> Seguí tu compra  </li>
 
           <li><PiMapPinFill className='w-5 h-6 text-venex' /></li>
           <li className='pr-14 pl-3'> Retirá tu compra </li>
