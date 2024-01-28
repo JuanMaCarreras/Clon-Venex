290cfd024a6d4b30eed3e5de87b6752eacda90c1
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-27 21:51:27 -0300

    font-family

--- client/src/Components/FilterNavBar.jsx
+++ client/src/Components/FilterNavBar.jsx
@@ -1,16 +1,44 @@
 function FilterNavBar () {
   return (
     <>
-      <ul className='flex flex-row justify-between py-16px px-14'>
-        <li className='font-semibold'>CATEGORÍAS</li>
-        <li>PCs de Escritorio</li>
-        <li>Notebooks</li>
-        <li>Placas de Videos</li>
-        <li>Monitores</li>
-        <li>Micros</li>
-        <li>¡Imperdibles!</li>
-        <li>| Arma tu PC |</li>
-        <li className='font-semibold'>VX | CORPORATIVO</li>
+      <ul className='flex flex-row justify-between py-16px px-14 bg-white'>
+        <li
+          className='font-semibold border-r border-solid border-gray-300 pr-5'
+        >CATEGORÍAS
+        </li>
+        <li
+          className='font-montserrat text-font '
+        >PCs de Escritorio
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >Notebooks
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >Placas de Videos
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >Monitores
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >Micros
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >¡Imperdibles!
+        </li>
+        <li
+          className='font-montserrat text-font'
+        >
+          | Arma tu PC |
+        </li>
+        <li
+          className='font-semibold font-montserrat'
+        >VX | CORPORATIVO
+        </li>
       </ul>
     </>
   )
