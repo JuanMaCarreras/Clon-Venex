03d856afd1c886ee05c66b433d9d2d3972575ccc
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-28 20:49:21 -0300

    style

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,4 +1,6 @@
 import SearchBar from './SearchBar'
+import { FaRegHeart, FaShoppingCart } from 'react-icons/fa'
+
 import TopBar from './TopBar'
 
 function NavBar () {
@@ -6,12 +8,21 @@ function NavBar () {
     <>
 
       <TopBar />
-      <nav className=' bg-primaryGray items-center '>
-        <div>
+      <nav className=' bg-primaryGray flex flex-row items-center justify-around '>
+        <div className=''>
+          <img
+            src='https://www.venex.com.ar/fil/logo/header_logo_nuevo.png'
+            alt='#'
+            className='h-24 w-72 object-cover flex-shrink-0'
+          />
+        </div>
+        <div className=''>
           <SearchBar />
         </div>
-        <ul className='flex  justify-end'>
-          <li className='text-white'>Usuario</li>
+        <ul className='flex flex-row items-center pr-2'>
+          <li className='text-gray-100 pr-5 font-montserrat'>Bienvenida/o Usuario</li>
+          <li className='text-gray-300 pr-5 text-2xl'><FaRegHeart /></li>
+          <li className='text-gray-300 text-3xl'><FaShoppingCart /></li>
         </ul>
       </nav>
     </>
