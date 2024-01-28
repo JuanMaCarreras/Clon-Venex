9f7c0fab059ffe4262c3f2c90b8abbb4ff1b5cce
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-27 21:51:53 -0300

    add autoplay

--- client/src/Components/SliderImg.jsx
+++ client/src/Components/SliderImg.jsx
@@ -1,14 +1,20 @@
+import { useEffect } from 'react'
 import { register } from 'swiper/element/bundle'
 import Banner1 from '../assets/img/Banner1.png'
 import Banner2 from '../assets/img/Banner2.png'
 import Banner3 from '../assets/img/Banner3.png'
 
 function SliderImg () {
-  register()
+  useEffect(() => {
+    register()
+  }, [])
   return (
     <>
       <swiper-container
+        slides-per-view='1'
         loop='true'
+        autoplay-delay='4000'
+        autoplay-disable-on-interaction='false'
       >
         <swiper-slide>
           <img src={Banner1} className='max-h-300px w-screen' />
