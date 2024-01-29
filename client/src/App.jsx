bc8d1dad488c17e341eed7a432963e797dccabfc
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-29 11:29:47 -0300

    app new route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,16 @@ import CreateProducts from './components/CreateProduct'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import ProductDetails from './components/ProductDetails.jsx'
+import HelpCenter from './components/HelpCenter.jsx'
 
 function App () {
   return (
     <>
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/create_products' element={<CreateProducts />} />
+        <Route path='/create-products' element={<CreateProducts />} />
         <Route path='/product/:id' element={<ProductDetails />} />
+        <Route path='/centro-de-ayuda' element={<HelpCenter />} />
       </Routes>
     </>
   )
