43c495892fb8b16a382705e82aa03a4db1236c6f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-01-29 11:32:28 -0300

    help center link

--- client/src/Components/TopBar.jsx
+++ client/src/Components/TopBar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import { BsHeadset } from 'react-icons/bs'
 import { FaRoute } from 'react-icons/fa'
 import { PiMapPinFill } from 'react-icons/pi'
@@ -8,15 +9,16 @@ function TopBar () {
       <div
         className='flex flex-row justify-end py-1.5 bg-secondaryGray'
       >
-        <ul className='flex text-white'>
+        <ul className='flex text-white font-montserrat'>
           <li><FaRoute className='w-4 h-6 text-venex ' /></li>
-          <li className='pr-11 pl-3 text-15px font-montserrat'> Seguí tu compra  </li>
+          <li className='pr-11 pl-3 text-15px '> Seguí tu compra  </li>
 
           <li><PiMapPinFill className='w-5 h-6 text-venex' /></li>
-          <li className='pr-11 pl-3 text-15px font-montserrat'> Retirá tu compra </li>
-
+          <li className='pr-11 pl-3 text-15px '> Retirá tu compra </li>
           <li><BsHeadset className='w-5 h-6 text-venex' /></li>
-          <li className='pr-9 pl-3 text-15px font-montserrat'>Centro de Ayuda</li>
+          <NavLink to='/centro-de-ayuda'>
+            <li className='pr-9 pl-3 text-15px '>Centro de Ayuda</li>
+          </NavLink>
         </ul>
 
       </div>
