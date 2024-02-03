98ad4717f265a5092ef5376bd785fd6afa0b9aea
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-02 23:07:01 -0300

    permanent NavBar and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,22 @@
 import CreateProducts from './components/CreateProduct'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
-import ProductDetails from './components/ProductDetails.jsx'
-import HelpCenter from './components/HelpCenter.jsx'
+import ProductDetails from './components/ProductDetails'
+import HelpCenter from './components/HelpCenter'
+import Footer from './components/Footer'
+import NavBar from './components/NavBar'
 
 function App () {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route path='/create-products' element={<CreateProducts />} />
         <Route path='/products/:productId' element={<ProductDetails />} />
         <Route path='/centro-de-ayuda' element={<HelpCenter />} />
       </Routes>
+      <Footer />
     </>
   )
 }
