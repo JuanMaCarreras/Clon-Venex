247c5a89c1db3d6f924f49a40d3758edca09aaa9
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-02 23:12:14 -0300

    restore without navbar and without footer

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,18 +1,12 @@
-import Footer from './Footer'
 import CardList from './CardList'
-import NavBar from './NavBar'
-import FilterNavBar from './FilterNavBar'
 import SliderImg from './SliderImg'
 
 function Home () {
   return (
     <>
-      <NavBar />
-      <FilterNavBar />
       <SliderImg />
       <CardList />
       <h1 className='font-hook text-2xl pb-96' />
-      <Footer />
     </>
   )
 }
