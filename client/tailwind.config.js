ff88cd53fda88618b47db082b15a9f753537fbe2
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-04 21:35:03 -0300

    font size config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -18,10 +18,16 @@ export default {
         '300px': '353px',
         '41px': '42px',
         '15px': '15px',
-        '600px': '593px'
+        '600px': '593px',
+        '30rem': '33rem',
+        '36rem': '36rem'
+
       },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serif']
+      },
+      fontSize: {
+        detailName: '1.15rem' // Aquí defines tu propio tamaño de fuente personalizado
       }
 
     },
