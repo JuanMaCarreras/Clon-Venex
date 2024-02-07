c3b044d4b552527e5e8df73ae0f39cade32d02f9
JuanMaCarrearas <manuucarreras@gmail.com>
2024-02-06 23:23:47 -0300

    auth0 route

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -8,6 +8,10 @@ import {
     getBrand,
 } from '../controllers/products.controller.js'
 
+import pkg from 'express-openid-connect'
+
+const { requiresAuth } = pkg
+
 const router = Router()
 
 router.get('/products', getAllProducts)
@@ -18,5 +22,13 @@ router.get('/products/brand/:brand', getBrand)
 router.post('/products', createProducts)
 router.put('/products/:id')
 router.delete('/products/:id')
+
+
+//Auth0 rute
+
+router.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.openid.user))
+})
+
 
 export default router
