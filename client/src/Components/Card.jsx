a4541f2135b0b098dddef5c3ce3fd3939b3b4085
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-10 18:52:53 -0300

    Added styling to card component

--- client/src/Components/Card.jsx
+++ client/src/Components/Card.jsx
@@ -10,12 +10,14 @@ function Card ({ product }) {
   return (
     <>
       <NavLink to={`/products/${id}`}>
-        <article className=' flex flex-col justify-center m-5 w-80 border-2 rounded-lg'>
+        <article
+          className='flex flex-col items-center justify-center m-5 w-80 border-gray-300 border-[1px] rounded-lg hover:border-venex'
+        >
           <img src={img} alt={name} className='h-60 w-60' />
 
-          <div className=' pt-4 pb-2 px-8'>
+          <div className='pt-4 pb-2 px-8'>
             <h3 className='text-sm text-center text-gray-700 break-words'>{name}</h3>
-            <h2 className='text-2xl font-medium'>${price}</h2>
+            <h2 className='text-2xl font-medium py-2'>${price}</h2>
           </div>
         </article>
       </NavLink>
