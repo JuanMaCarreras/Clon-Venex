806fbe971d4601105f07aefeaa2af43014fe1732
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-14 18:31:40 -0300

    use alias imports for product controllers in routes

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -1,22 +1,15 @@
 import { Router } from 'express'
-import {
-    getAllProducts,
-    createProducts,
-    searchProducts,
-    getProductsById,
-    getCategory,
-    getBrand,
-} from '../controllers/products.controller.js'
+import * as productsCtrl from '../controllers/products.controller.js'
 
 
 const router = Router()
 
-router.get('/products', getAllProducts)
-router.get('/products/:id', getProductsById)
-router.get('/products/:name', searchProducts)
-router.get('/products/category/:category', getCategory)
-router.get('/products/brand/:brand', getBrand)
-router.post('/products', createProducts)
+router.get('/products', productsCtrl.getAllProducts)
+router.get('/products/:id', productsCtrl.getProductsById)
+router.get('/products/:name', productsCtrl.searchProducts)
+router.get('/products/category/:category', productsCtrl.getCategory)
+router.get('/products/brand/:brand', productsCtrl.getBrand)
+router.post('/products', productsCtrl.createProducts)
 router.put('/products/:id')
 router.delete('/products/:id')
 
