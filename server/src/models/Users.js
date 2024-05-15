a9196e917bea60857ba1f22774b59573676cd612
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-15 17:57:12 -0300

    Define entity relationship between users and roles

--- server/src/models/Users.js
+++ server/src/models/Users.js
@@ -18,7 +18,7 @@ const Users = sequelize.define('users', {
     img: {
         type: DataTypes.STRING,
         defaultValue: 'https://icon-library.com/images/default-user-icon/default-user-icon-13.jpg'
-    }
+    },
 })
 
 export default Users
