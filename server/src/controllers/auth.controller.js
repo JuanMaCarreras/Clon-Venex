d76d13b307ff63ae1ff788498adeb736bdfc45d7
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-16 18:33:28 -0300

    encrypted password

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,13 +1,32 @@
+import bcrypt from 'bcrypt'
+import Users from '../models/Users.js'
+// import Roles from '../models/Roles.js'
+
+export const signUp = async (req, res) => {
+
+    const { name, email, password, role } = req.body
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+
+        const user = await Users.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        console.log(user)
 
 
+        res.json('Todo ok rey')
 
-export const signUp = (req, res) => {
+    } catch (error) {
 
-    const { name, email, password } = req.body
+        console.log(error)
+        res.status(500).json({ error: 'Error al Registrarse ' })
 
-    console.log(req.body)
-
-    res.json('Sign Up')
+    }
 }
 
 export const signIn = (req, res) => {
