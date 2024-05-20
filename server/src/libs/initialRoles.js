017f4d64177a9b7baf8bf6ce3e153d4cd2072fd3
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-20 19:39:22 -0300

    Add createRoles function to verify and create roles

--- server/src/libs/initialRoles.js
+++ server/src/libs/initialRoles.js
@@ -1,10 +1,24 @@
+import Roles from "../models/Roles"
+
 export const initialRoles = async () => {
 
     try {
+        const count = await Roles.count()
+
+        if (count > 0) return
+
+        const roles = ['user', 'moredator', 'admin']
 
 
+        const rolsePromise = roles.map(roleName => Roles.create({ name: roleName }))
+
+        await Promise.all(rolsePromise)
+
+        console.log('Roles have been created')
 
     } catch (error) {
+
+        console.log(error)
 
     }
 
