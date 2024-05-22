dd6157d769101fbc6117a9d6a73e3bda5b935f42
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-22 17:48:36 -0300

    Add login logic

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,27 +1,35 @@
 import bcrypt from 'bcrypt'
 import Users from '../models/Users.js'
-// import Roles from '../models/Roles.js'
+import Roles from '../models/Roles.js'
 import jwt from 'jsonwebtoken'
 import config from '../config.js'
 
 export const signUp = async (req, res) => {
 
-    const { name, email, password, role } = req.body
+    const { name, email, password, roles } = req.body
 
     const hashedPassword = await bcrypt.hash(password, 10)
 
     try {
 
-        const user = await Users.create({
+        const newUser = await Users.create({
             name,
             email,
             password: hashedPassword
         })
 
-        console.log(user)
+        console.log(newUser)
 
-        const token = jwt.sign({ id: user.id }, config.SECRET, { expiresin: 86400 })
+        const token = jwt.sign({ id: newUser.id }, config.SECRET, { expiresin: 86400 })
 
+
+        if (roles) {
+            const findRoles = await Roles.find({ where: { name: roles } })
+            newUser.roles = findRoles.map(role => role.id)
+
+        } else {
+
+        }
 
 
         res.status(200).json({ token })
@@ -34,11 +42,15 @@ export const signUp = async (req, res) => {
     }
 }
 
-export const signIn = (req, res) => {
+export const signIn = async (req, res) => {
 
     const { email, password } = req.body
 
-    console.log(email, password)
+    const user = await Users.findOne({ email })
+
+    if (!user) {
+        res.status(400).json({ error: 'User Not Found' })
+    }
 
 
     res.json('Sign In')
