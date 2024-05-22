3acd7088d868f6f5203b89f613396e0cbad0c10f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-22 17:47:22 -0300

    index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import app from './src/app.js'
 import sequelize from './src/db.js'
 import './src/models/Products.js'
-import { createRoles } from './src/libs/initialRoles.js'
 
 sequelize.sync({ force: true }).then(async () => {
     console.log('Data base connected')
@@ -9,5 +8,4 @@ sequelize.sync({ force: true }).then(async () => {
     app.listen(3000, () => {
         console.log('Server lintener on port', 3000)
     })
-    await createRoles()
 })
