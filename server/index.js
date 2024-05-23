82179021adf615c5c42a06238ddb9b5625493493
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-23 17:57:54 -0300

    Invoke createRoles in index (DB initialization)

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import app from './src/app.js'
 import sequelize from './src/db.js'
+import { createRoles } from './src/libs/initialRoles.js'
 import './src/models/Products.js'
 
 sequelize.sync({ force: true }).then(async () => {
     console.log('Data base connected')
-
+    createRoles()
     app.listen(3000, () => {
         console.log('Server lintener on port', 3000)
     })
