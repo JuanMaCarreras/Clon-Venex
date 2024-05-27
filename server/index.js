ce75b88acfd9e935b89d6834a09a2ec0ba02d10c
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-27 17:27:43 -0300

    Change sequelize sync force option

--- server/index.js
+++ server/index.js
@@ -1,11 +1,9 @@
 import app from './src/app.js'
 import sequelize from './src/db.js'
-import { createRoles } from './src/libs/initialRoles.js'
 import './src/models/Products.js'
 
-sequelize.sync({ force: true }).then(async () => {
+sequelize.sync({ force: false }).then(async () => {
     console.log('Data base connected')
-    createRoles()
     app.listen(3000, () => {
         console.log('Server lintener on port', 3000)
     })
