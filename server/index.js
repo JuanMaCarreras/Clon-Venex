846ae526bd6b85f29e78bd28501e1e956d3abda3
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-28 19:11:59 -0300

    index

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import app from './src/app.js'
 import sequelize from './src/db.js'
 import './src/models/Products.js'
 
-sequelize.sync({ force: false }).then(async () => {
+sequelize.sync({ force: true }).then(async () => {
     console.log('Data base connected')
     app.listen(3000, () => {
         console.log('Server lintener on port', 3000)
