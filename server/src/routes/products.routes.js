3e18c65fc10d7631230a41cefe0befcde01ad5d9
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-29 18:08:39 -0300

    Add token verification and role-based route protection

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import * as productsCtrl from '../controllers/products.controller.js'
-
+import { verifyToken } from '../middleware/auth.jwt.js'
 
 const router = Router()
 
@@ -9,7 +9,8 @@ router.get('/products/:id', productsCtrl.getProductsById)
 router.get('/products/:name', productsCtrl.searchProducts)
 router.get('/products/category/:category', productsCtrl.getCategory)
 router.get('/products/brand/:brand', productsCtrl.getBrand)
-router.post('/products', productsCtrl.createProducts)
+
+router.post('/products', verifyToken, productsCtrl.createProducts)
 router.put('/products/:id', productsCtrl.updateProductsById)
 router.delete('/products/:id')
 
