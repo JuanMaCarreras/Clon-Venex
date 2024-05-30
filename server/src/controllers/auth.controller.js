e4644f3a1e1fba2cab2d46ef58bb83c86879aefe
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-30 17:02:49 -0300

    Complete auth controller functions for registration and login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -3,7 +3,6 @@ import Users from '../models/Users.js'
 import { tokenSign } from '../helpers/token.jwt.js'
 
 export const signUp = async (req, res) => {
-
     const { name, email, password, role } = req.body
 
     const hashedPassword = await bcrypt.hash(password, 10)
@@ -26,7 +25,7 @@ export const signUp = async (req, res) => {
     } catch (error) {
 
         console.log(error)
-        res.status(500).json({ error: 'Error al Registrarse ' })
+        res.status(500).json({ error: 'Error registering' })
 
     }
 }
@@ -50,7 +49,8 @@ export const signIn = async (req, res) => {
         res.status(200).json({ token })
 
     } catch (error) {
-
+        console.log(error)
+        res.status(500).json({ error: 'Login error' })
     }
 
 }
