46fe68cd8f66406ff39d6146f1ee426f6f595f66
JuanMaCarrearas <manuucarreras@gmail.com>
2024-05-31 18:39:02 -0300

    Add validateSignUp function

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -9,11 +9,7 @@ export const validateSignUp = [
 
     (req, res, next) => {
 
-        const errors = validationResult(req)
 
-        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
-
-        next()
 
     }
 
