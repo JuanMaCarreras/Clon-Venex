e0e357b73b6d0f127de2d31e6c8582bddad1e47f
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-04 18:53:36 -0300

    App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,20 +3,22 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import ProductDetails from './components/ProductDetails'
 import HelpCenter from './components/HelpCenter'
-import Footer from './components/Footer'
-import NavBar from './components/NavBar'
+// import Footer from './components/Footer'
+// import NavBar from './components/NavBar'
+import Register from './components/Register.jsx'
 
 function App () {
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route exact path='/register' element={<Register />} />
         <Route path='/create-products' element={<CreateProducts />} />
         <Route path='/products/:productId' element={<ProductDetails />} />
         <Route path='/centro-de-ayuda' element={<HelpCenter />} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
