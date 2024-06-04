880d08fbcd2eb04fdd1c3c3c4b4d85f81380eec2
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-04 18:51:45 -0300

    Add reducer for user registration

---
+++ client/src/redux/slices/authSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit'
+import * as authFn from '../actions/authActions'
+
+const initialState = {
+  isAutehticator: false,
+  data: [],
+  toke: null,
+  status: null,
+  error: null
+
+}
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(authFn.registerUser.fulfilled, (state, action) => {
+        state.isAutehticator = true
+        state.data = action.payload.data
+        state.toke = action.payload.toke
+      })
+      .addCase(authFn.registerUser.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.payload
+      })
+  }
+})
+
+export default authSlice.reducer
