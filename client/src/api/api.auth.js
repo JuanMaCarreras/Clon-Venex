bb0d2c87c38c10da1e6fb13a91ef69f1e6e7cbff
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-05 19:12:45 -0300

    Add auth file with backend calls to api folder

--- client/src/api/api.auth.js
+++ client/src/api/api.auth.js
@@ -6,3 +6,8 @@ export const registerUser = async userData => {
   const response = await axios.post(`${BASE_URL}/auth/signup`, userData)
   return response.data
 }
+
+export const loginUser = async userData => {
+  const response = await axios.post(`${BASE_URL}/auth/signin`, userData)
+  return response.data
+}
