d2745af3e0a778cee1a12e3f1e2e700465e6af4d
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-07 17:37:56 -0300

    Add validateSignIn to sign-in route

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'
 import * as authCtrl from '../controllers/auth.controller.js'
-import { validateSignUp } from '../validators/auth.validator.js'
+import { validateSignUp, validateSignIn } from '../validators/auth.validator.js'
 
 const router = Router()
 
 router.post('/auth/signup', validateSignUp, authCtrl.signUp)
-router.post('/auth/signin', authCtrl.signIn)
+router.post('/auth/signin', validateSignIn, authCtrl.signIn)
 
 
 
