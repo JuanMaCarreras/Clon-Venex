7916da0c9c4dfa2da02ff894ac2c3956b949bc46
JuanMaCarrearas <manuucarreras@gmail.com>
2024-06-07 17:38:29 -0300

    Add validateSignIn function

--- server/src/validators/auth.validator.js
+++ server/src/validators/auth.validator.js
@@ -18,3 +18,19 @@ export const validateSignUp = [
     }
 
 ]
+
+export const validateSignIn = [
+    check('email').isEmail().withMessage('Email is required'),
+    check('password').isLength({ min: 8 }).withMessage('Password must be at least 6 characters long'),
+    check('role').optional(),
+
+    (req, res, next) => {
+
+        const errors = validationResult(req)
+
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() })
+
+        next()
+    }
+
+]
